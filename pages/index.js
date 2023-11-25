4eac020f7ec409661e39651f474e443434a6fe52
Rafael Molina Martinez <33470634+martinezrafael@users.noreply.github.com>
2023-11-25 20:41:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Aline, eu te amo. Se você também me ama da uma risadinha!</h1>
+function Home() {
+  return <h1>Aline, eu te amo. Se você também me ama da uma risadinha!</h1>;
 }
 
 export default Home;
