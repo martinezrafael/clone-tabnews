425cb917b1fc4080a079394f4c40fc7621827b15
Rafael Molina Martinez <33470634+martinezrafael@users.noreply.github.com>
2023-12-27 21:14:02 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res){
-    res.status(200).json({chave: "São acima da média"})
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  res.status(200).json({ chave: "São acima da média" });
 }
 
 export default status;
